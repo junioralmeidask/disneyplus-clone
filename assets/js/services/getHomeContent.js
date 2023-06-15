fddc1358fbe18a50d689831d2b9fb20fc1a8411d
Fco Florencio de Almeida Junior <104158278+junioralmeidask@users.noreply.github.com>
2023-06-15 19:10:25 -0300

    Update getHomeContent.js

--- assets/js/services/getHomeContent.js
+++ assets/js/services/getHomeContent.js
@@ -1,6 +1,6 @@
 export const getHomeContent = () => {
     return new Promise((resolve, reject) => {
-        fetch('/content/data.json')
+        fetch('../../../content/data.json')
         .then((response) => {
             return response.json()
         })
